8b47a169c87ca8b0435ab1e20a1e3183198c68ad
PanchoF1 <fsadams@hotmail.com>
2024-08-23 23:45:47 -0300

    corrijo color fichas

--- board.py
+++ board.py
@@ -21,14 +21,14 @@ class Board:
         self.__positions__[0][6] = Knight("WHITE") 
         self.__positions__[0][7] = Rook("WHITE") 
 
-        self.__positions__[7][0] = Rook("WHITE") # black posiciones
-        self.__positions__[7][1] = Knight("WHITE") 
-        self.__positions__[7][2] = Bishop("WHITE") 
-        self.__positions__[7][3] = King("WHITE") 
-        self.__positions__[7][4] = Queen("WHITE")         
-        self.__positions__[7][5] = Bishop("WHITE") 
-        self.__positions__[7][6] = Knight("WHITE") 
-        self.__positions__[7][7] = Rook("WHITE")
+        self.__positions__[7][0] = Rook("BLACK") # black posiciones
+        self.__positions__[7][1] = Knight("BLACK") 
+        self.__positions__[7][2] = Bishop("BLACK") 
+        self.__positions__[7][3] = King("BLACK") 
+        self.__positions__[7][4] = Queen("BLACK")         
+        self.__positions__[7][5] = Bishop("BLACK") 
+        self.__positions__[7][6] = Knight("BLACK") 
+        self.__positions__[7][7] = Rook("BLACK")
 
     def __str__(self):
         board_str = ""
