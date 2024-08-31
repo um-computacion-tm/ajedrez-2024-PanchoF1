d194a2f338a8431cb9f8c1c86d684aaa3ed767bf
PanchoF1 <fsadams@hotmail.com>
2024-08-31 01:39:30 -0300

    agrego test move vertical asc

--- test_rook.py
+++ test_rook.py
@@ -4,7 +4,7 @@ from board import Board
 
 class TestRook(unittest.TestCase):
 
-     def test_move_vertical_desc(self):
+    def test_move_vertical_desc(self):
         board = Board()
         rook = Rook("WHITE", board)
         possibles = rook.possible_positions_vd(4, 1)
@@ -13,6 +13,14 @@ class TestRook(unittest.TestCase):
             [(5, 1), (6, 1), (7, 1)]
         )
 
-
+    def test_move_vertical_asc(self):
+        board = Board()
+        rook = Rook("WHITE", board)
+        possibles = rook.possible_positions_va(4, 1)
+        self.assertEqual(
+            possibles,
+            [(3, 1), (2, 1), (1, 1), (0, 1)]
+        )
+    
 if __name__ == '__main__':
     unittest.main()
