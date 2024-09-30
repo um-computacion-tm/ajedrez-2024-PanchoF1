81a4a1bdb6a9c0e1c9945bcac92e86a385f1edab
PanchoF1 <fsadams@hotmail.com>
2024-09-29 23:48:34 -0300

    agrego possibles vd va

--- piece.py
+++ piece.py
@@ -30,3 +30,21 @@ class Piece:
             self.possible_positions_vd(from_row, from_col) +
             self.possible_positions_va(from_row, from_col)
         )    
+
+    def possible_positions_vd(self, row, col):
+        possibles = []
+        for next_row in range(row + 1, 8):
+            # que la celda que sigue no este ocupada..
+            other_piece = self.__board__.get_piece(next_row, col)
+            if other_piece is not None:
+                if other_piece.__color__ != self.__color__:
+                    possibles.append((next_row, col))
+                break
+            possibles.append((next_row, col))
+        return possibles
+
+    def possible_positions_va(self, row, col):
+        possibles = []
+        for next_row in range(row - 1, -1, -1):
+            possibles.append((next_row, col))
+        return possibles    
