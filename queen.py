6c754deeedc0c30e1dc89b58a6e5da9202301a89
PanchoF1 <fsadams@hotmail.com>
2024-10-06 23:50:52 -0300

    agrego metodo get_possible_positions

--- queen.py
+++ queen.py
@@ -6,3 +6,11 @@ class Queen(Piece):
     white_str = "♛"
     black_str = "♕"
 
+    def get_possible_positions(self, from_row, from_col):
+        return self.possible_orthogonal_positions(
+            from_row,
+            from_col,
+        ) + self.possible_diagonal_positions(
+            from_row,
+            from_col,
+        )
